a3c4934974f5b14ea13d4194b03b649fa8a29ed8
Дмитрий <1primo1@mail.ru>
2025-06-26 22:42:12 +0300

    start event

--- src/js/libs/slider.js
+++ src/js/libs/slider.js
@@ -63,5 +63,64 @@ const paphosPrimaryslider = new Swiper('.paphos__primary-slider', {
 
 });
 
+const paphosEventsSlider = new Swiper('.paphos-events__slider', {
+    // Стрелки
+    navigation: {
+        nextEl: '#paphos-events__slider-btn-next',
+        prevEl: '#paphos-events__slider-btn-prev',
+    },
+    pagination: {
+        el: '#paphos-events__pagination',
+        clickable: true
+      },
+      centeredSlides: true,
+
+    /*Отступ у карточек*/
+    spaceBetween: 20,
+    /*Показывать по n карточек*/
+    slidesPerView: 1,
+    /* При достижении конца, перепрыгнуть в начало */
+    // rewind: true,
+    /*Увеличение при наведении курсора мыши */
+    // zoom: true,
+    /*Ленивая подгрузка */
+    // lazy: true,
+    /*Бесконечная прокрутка */
+    // loop: true,
+    /*Ориентация */
+    // direction: 'vertical',
+    /*Авто высота*/
+    // autoHeight: true,
+    /*иконка захвата при наведении на слайд*/
+    grabCursor: true,
+    // firstSlideToShow: 1,
+    // initialSlide: 1,
+    /*Автоматическое перелистывание*/
+    // autoplay: {
+    //     delay: 3000,
+    //   },
+    //   speed: 700,
+    /*Брек-поинты*/
+    // breakpoints: {
+    //     1220: {
+    //         slidesPerView: 3,
+    //         spaceBetween: 240,
+    //     },
+    //     992: {
+    //         slidesPerView: 3,
+    //         spaceBetween: 140,
+    //     },
+    //     768: {
+    //         slidesPerView: 2,
+    //         spaceBetween: 100,
+    //     },
+    //     568: {
+    //         slidesPerView: 2,
+    //         spaceBetween: 20,
+    //     }
+    // },
+
+});
+
 
 
